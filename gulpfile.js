61353534f1cfebdc5a10cab30d2c82060115ea10
Naman Jindal <namanjindal113@gmail.com>
2024-09-07 20:40:32 +0530

    gulp code

---
+++ gulpfile.js
@@ -0,0 +1,10 @@
+
+
+import gulp from 'gulp';
+import imagemin from 'gulp-imagemin';
+
+gulp.task('default', () => {
+  return gulp.src("images/*")
+    .pipe(imagemin())
+    .pipe(gulp.dest('dist/images'))
+});
